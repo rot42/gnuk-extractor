4fea0e14dc88ece1494e6e594ae400696322f049
rot42 <rot42@protonmail.com>
2019-08-02 23:14:16 +0200

    replaced PGPy submodule with new PyPI version
    Curve25519 and Ed25519 support, and S2K GNU extensions support have
    been merged into upstream PGPy.

--- setup.py
+++ setup.py
@@ -5,20 +5,11 @@ from setuptools import setup
 from setuptools.command.install import install
 from pip._internal import main as pip
 
-# Ugly hack to install local version of PGPy instead of PyPI version
-# Could not find a clean way to do it with setuptools
-class InstallPGPy(install):
-    def run(self):
-        install.run(self)
-        pip(['install', './PGPy'])
-
 setup(
     name='gnukextract',
     version='1.0.0',
     description='Extract PGP private key from Gnuk / Nitrokey Start firmware',
     packages=['gnukextract'],
     scripts=['gnuk-extractor'],
-    # TODO: replace once PGPy pull requests are merged and new version is published on PyPI
-    #install_requires=['PGPy == 0.4.?'],
-    cmdclass={'install': InstallPGPy}
+    install_requires=['PGPy == 0.5.2'],
 )
