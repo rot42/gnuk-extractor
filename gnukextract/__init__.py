5a075966faf1298eac9827387cfea3eb3f49dfa1
rot42 <rot42@protonmail.com>
2019-08-04 11:56:14 +0200

    completed missing info in setup.py ; incremented version to 1.0.1

--- gnukextract/__init__.py
+++ gnukextract/__init__.py
@@ -1,3 +1,5 @@
 from .gnukextract import Firmware
 
+__version__ = '1.0.1'
+__author__ = 'rot42'
 __all__ = ['Firmware']
--- setup.py
+++ setup.py
@@ -6,11 +6,17 @@ from setuptools.command.install import install
 from pip._internal import main as pip
 
 setup(
-    name='gnukextract',
-    version='1.0.0',
+    name='gnuk-extractor',
+    version='1.0.1',
     description='Extract PGP private key from Gnuk / Nitrokey Start firmware',
+    url='https://github.com/rot42/gnuk-extractor',
+    author='rot42',
+    license='AGPLv3',
     packages=['gnukextract'],
     install_requires=['PGPy == 0.5.2'],
+    extras_require={
+        'test': ['pytest'],
+    },
     entry_points={
         'console_scripts': [
             'gnuk-extractor = gnukextract.command_line:main',
